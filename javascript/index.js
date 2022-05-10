c58cd1800dcd405114de58f15ff03078d7c3f481
Lokendra Puri <lpuri1979@gmail.com>
2022-05-10 21:49:37 +0100

    Done

--- javascript/index.js
+++ javascript/index.js
@@ -14,24 +14,30 @@ const milUniElement = document.getElementById('milUni');
 const splitsElement = document.getElementById('splits');
 
 function printTime() {
-  // ... your code goes here
+  minDecElement.innerHTML = 'minDec';
+  return ("0" + minDec).slice(-2)
 }
 
 function printMinutes() {
   // ... your code goes here
+  // minDecElement.innerHTML = this.computeTwoDigitNumber(this.minDec());
+
 }
 
 function printSeconds() {
   // ... your code goes here
+  secDecElement = this.computeTwoDigitNumber(this.getSeconds());
 }
 
 // ==> BONUS
 function printMilliseconds() {
   // ... your code goes here
+
 }
 
 function printSplit() {
   // ... your code goes here
+
 }
 
 function clearSplits() {
@@ -40,23 +46,37 @@ function clearSplits() {
 
 function setStopBtn() {
   // ... your code goes here
+  btnLeftElement.innerHTML = 'STOP';
+  btnRightElement.className = 'btn STOP';
 }
 
 function setSplitBtn() {
   // ... your code goes here
+  btnRightElement.innerHTML = 'SPLIT';
+  btnRightElement.className = 'btn split';
 }
 
 function setStartBtn() {
-  // ... your code goes here
+  btnLeftElement.innerHTML = 'START';
+  btnLeftElement.className = 'btn start';
 }
 
 function setResetBtn() {
   // ... your code goes here
+  btnRightElement.innerHTML = 'RESET';
+  btnRightElement.className = 'btn reset';
 }
 
 // Start/Stop Button
 btnLeftElement.addEventListener('click', () => {
   // ... your code goes here
+  if(btnLeftElement.innerHTML === 'START') {
+    setStopBtn();
+    setSplitBtn();
+  }else {
+    setStartBtn();
+    setResetBtn();
+  }
 });
 
 // Reset/Split Button
